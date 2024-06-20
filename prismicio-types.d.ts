bcabb1e504e02f20a0bbbc4a345aa8091184d2f8
Kreya <crashaw@gmail.com>
2024-06-20 15:19:16 +0200

    added about slices

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -5,6 +5,7 @@ import type * as prismic from "@prismicio/client";
 type Simplify<T> = { [KeyType in keyof T]: T[KeyType] };
 
 type AboutUsDocumentDataSlicesSlice =
+  | ContentSlice
   | GallerySlice
   | ImageSliderSlice
   | HeroSlice;
