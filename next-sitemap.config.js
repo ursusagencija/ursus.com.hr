6c7bfdf2ab3b4cdaae6424328ef30cc208a3306a
Kreya <crashaw@gmail.com>
2024-11-12 14:18:20 +0100

    added sitemap builder

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteUrl: process.env.SITE_URL,
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+  exclude: ["/api/*", "/slice-simulator", "/message-sent"],
+};
