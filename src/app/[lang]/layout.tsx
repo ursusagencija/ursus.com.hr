51ccfaeeb7ff01adcbe3262299b8e5956b692888
Kreya <crashaw@gmail.com>
2024-11-14 11:31:03 +0100

    added gtm

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,12 +1,16 @@
 import "@/styles/globals.css";
 import { Jost, Playfair_Display, Satisfy } from "next/font/google";
 import Header from "@/components/Header";
+import Script from "next/script";
 
 // Import Swiper styles
 import "swiper/css";
 import Footer from "@/components/Footer";
 import ContactBar from "@/components/ContactBar";
 import SearchProvider from "@/providers/SearchProvider";
+
+//GTM
+const GTM_ID = "G-MYEM5VR86R";
 
 /*Configure Google Fonts*/
 const jost = Jost({
@@ -39,6 +43,15 @@ export default function RootLayout({
       lang={params.lang}
       className={`${jost.variable} ${playfairDisplay.variable} ${satisfy.variable}`}
     >
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
       <body>
         <SearchProvider>
           <Header variant="transparent" />
@@ -46,6 +59,11 @@ export default function RootLayout({
           <Footer />
           <ContactBar number="385912004400" />
         </SearchProvider>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
       </body>
     </html>
   );
