753f7e0c99adeaf6692426dc3d4d7be6ddb6bf05
Kreya <crashaw@gmail.com>
2024-11-15 10:46:33 +0100

    dodan google analytics

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,16 +1,15 @@
 import "@/styles/globals.css";
 import { Jost, Playfair_Display, Satisfy } from "next/font/google";
 import Header from "@/components/Header";
-import Script from "next/script";
 
 // Import Swiper styles
 import "swiper/css";
 import Footer from "@/components/Footer";
 import ContactBar from "@/components/ContactBar";
 import SearchProvider from "@/providers/SearchProvider";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
-//GTM
-const GTM_ID = "G-MYEM5VR86R";
+
 
 /*Configure Google Fonts*/
 const jost = Jost({
@@ -43,15 +42,7 @@ export default function RootLayout({
       lang={params.lang}
       className={`${jost.variable} ${playfairDisplay.variable} ${satisfy.variable}`}
     >
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');
-        `}
-      </Script>
+      <GoogleAnalytics />
       <body>
         <SearchProvider>
           <Header variant="transparent" />
@@ -59,11 +50,7 @@ export default function RootLayout({
           <Footer />
           <ContactBar number="385912004400" />
         </SearchProvider>
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
-          }}
-        />
+
       </body>
     </html>
   );
